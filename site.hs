48a0a435bc0dd180d01b313427d832cadba470f4
Steven Lin <linjt267@outlook.com>
2022-10-19 04:15:24 +0800

    Add paginate

--- site.hs
+++ site.hs
@@ -4,8 +4,9 @@
 import Data.Monoid (mappend)
 import qualified Data.Set as S
 import Data.String (fromString)
-import Text.Pandoc.Options
 import Hakyll
+import Text.Pandoc.Options
+import Data.Tree (flatten)
 
 --------------------------------------------------------------------------------
 main :: IO ()
@@ -17,6 +18,8 @@ main = hakyll $ do
   match "css/*" $ do
     route idRoute
     compile compressCssCompiler
+
+  match "templates/*" $ compile templateBodyCompiler
 
   match (fromList ["about.org"]) $ do
     route $ setExtension "html"
@@ -37,7 +40,6 @@ main = hakyll $ do
             constField "title" title
               `mappend` listField "posts" postCtx (return posts)
               `mappend` defaultContext
-
       makeItem ""
         >>= loadAndApplyTemplate "templates/tag.html" ctx
         >>= loadAndApplyTemplate "templates/default.html" ctx
@@ -51,34 +53,34 @@ main = hakyll $ do
         >>= loadAndApplyTemplate "templates/default.html" postCtx
         >>= relativizeUrls
 
-  create ["archive.html"] $ do
-    route idRoute
-    compile $ do
-      posts <- recentFirst =<< loadAll "posts/*"
-      let archiveCtx =
-            listField "posts" postCtx (return posts)
-              `mappend` constField "title" "Archives"
-              `mappend` defaultContext
-
-      makeItem ""
-        >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-        >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-        >>= relativizeUrls
-
   match "index.html" $ do
     route idRoute
     compile $ do
       posts <- recentFirst =<< loadAll "posts/*"
       let indexCtx =
-            listField "posts" postCtx (return posts)
+            listField "posts" postCtx (return $ take 7 posts)
               `mappend` defaultContext
-
       getResourceBody
         >>= applyAsTemplate indexCtx
         >>= loadAndApplyTemplate "templates/default.html" indexCtx
         >>= relativizeUrls
 
-  match "templates/*" $ compile templateBodyCompiler
+  -- paginate
+  pag <- buildPaginateWith grouper "posts/*" makeId
+
+  paginateRules pag $ \pageNum pattern -> do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll pattern
+      let paginateCtx =
+            listField "posts" postCtx (return posts)
+              `mappend` constField "title" ("Page " ++ show pageNum)
+              `mappend` paginateContext pag pageNum
+              `mappend` defaultContext 
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/archive.html" paginateCtx
+        >>= loadAndApplyTemplate "templates/default.html" paginateCtx
+        >>= relativizeUrls
 
 --------------------------------------------------------------------------------
 postCtx :: Context String
@@ -109,12 +111,23 @@ tagsCtx =
 
 --------------------------------------------------------------------------------
 pandocMathCompiler =
-    let mathExtensions = [Ext_tex_math_dollars, Ext_tex_math_double_backslash,
-                          Ext_latex_macros]
-        defaultExtensions = writerExtensions defaultHakyllWriterOptions
-        newExtensions = defaultExtensions `mappend` pandocExtensions `mappend` extensionsFromList mathExtensions
-        writerOptions = defaultHakyllWriterOptions {
-                          writerExtensions = newExtensions,
-                          writerHTMLMathMethod = MathJax ""
-                        }
-    in pandocCompilerWith defaultHakyllReaderOptions writerOptions
+  let mathExtensions =
+        [ Ext_tex_math_dollars,
+          Ext_tex_math_double_backslash,
+          Ext_latex_macros
+        ]
+      defaultExtensions = writerExtensions defaultHakyllWriterOptions
+      newExtensions = defaultExtensions `mappend` pandocExtensions `mappend` extensionsFromList mathExtensions
+      writerOptions =
+        defaultHakyllWriterOptions
+          { writerExtensions = newExtensions,
+            writerHTMLMathMethod = MathJax ""
+          }
+   in pandocCompilerWith defaultHakyllReaderOptions writerOptions
+
+--------------------------------------------------------------------------------
+grouper :: (MonadMetadata m, MonadFail m) => [Identifier] -> m [[Identifier]]
+grouper = fmap (paginateEvery 10) . sortRecentFirst
+
+makeId :: PageNumber -> Identifier
+makeId pageNum = fromFilePath $ "posts/" ++ show pageNum ++ "/index.html"
